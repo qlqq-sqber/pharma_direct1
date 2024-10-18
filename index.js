7a03e0c3a1bfc68bd804855889f750ba826f8842
alaa <alaasaber22777@gmail.com>
2024-10-18 19:45:21 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+// let tellus = document.querySelector(".tellus");
+// let info = document.querySelector(".info");
+// let descripe = () =>{
+//     let bar = "" ;
+//     For(leti=0,i<100,i++);{
+//         bar =`
+//         <div class=”modal fade” id=”modal”>
+//             <div class=”modal-dialog”>
+//                 <div class=”modal-content”>
+//                     <div class=”modal-body”>
+//                     <input type=”text” placeholder=”ادخل اسمك”>
+//                     <br>
+//                     <input type=”number” placeholder=”رقم الواتساب “>
+//                     <br>
+//                     <input type=”text” placeholder=”اسم الدواء”>
+//                     </div>
+//                     <div class=”modal-footer”>
+//                         <button  data-bs-dismiss=”modal” data-bs-target=”#modal1”>ارسال</button>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>`;
+//     }
+//     Info.innerHTML = bar;
+// }
+// descripe();
